8ef8d12ba26bf225c75ba0a51d730c1f083f7e36
AngelsGV <angels.not99@gmail.com>
2023-12-09 20:34:27 +0100

    Initial commit

--- src/Resuelta5_4.java
+++ src/Resuelta5_4.java
@@ -2,15 +2,15 @@
 public class Resuelta5_4 {
 
     public static void main(String[] args) {
-        int[] valores; // o int valores[];
-        valores = new int[10];
+        int[] t; // o int valores[];
+       t = new int[10];
 
-        for (int i = 0; i < valores.length; i++) {
-            valores[i] = (int)(Math.random() * 100 + 1);
-            System.out.println(valores[i]); //Para que saque los valores aleatorios generados.
+        for (int i = 0; i < t.length; i++) {
+            t[i] = (int)(Math.random() * 100 + 1);
+            System.out.println(t[i]); //Para que saque los valores aleatorios generados.
         }
         int suma = 0; // Variable de tipo entero iniciada en 0.
-        for (int valor: valores){
+        for (int valor: t){
             //Se inicia un bucle for-each que itera a través de una colección llamada valores.
             //La colección debe ser de tipo iterable, como un array,
             //una lista o alguna otra estructura de datos que admita la iteración.
@@ -18,5 +18,17 @@ public class Resuelta5_4 {
             suma += valor;
         }
         System.out.println("La suma de los valores aleatorios es: " + suma);
+        maximo(t);
+    }
+    static int maximo(int t[]){
+        int max = t[0];
+        for (int e:t){
+            if (e > max) {
+                max = e;
+            }
+        }
+        System.out.println("El valor máximo es: " + max);
+        return (max);
+
     }
 }
