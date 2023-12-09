51022047e56c1f35bd7ae069b39655953cb3aac3
AngelsGV <angels.not99@gmail.com>
2023-12-09 12:53:51 +0100

    Initial commit

--- src/Resuelta5_1.java
+++ src/Resuelta5_1.java
@@ -1,14 +1,19 @@
 // Tabla de longitud 10, valores aleatorios de 1 a 100. Se mostrará suma de todos los valores.
 public class Resuelta5_1 {
     public static void main(String[] args) {
-        int valores[];
+        int[] valores; // o int valores[];
         valores = new int[10];
+
         for (int i = 0; i < valores.length; i++) {
             valores[i] = (int)(Math.random() * 100 + 1);
-
+            System.out.println(valores[i]); //Para que saque los valores aleatorios generados.
         }
-        int suma = 0;
+        int suma = 0; // Variable de tipo entero iniciada en 0.
         for (int valor: valores){
+            //Se inicia un bucle for-each que itera a través de una colección llamada valores.
+            //La colección debe ser de tipo iterable, como un array,
+            //una lista o alguna otra estructura de datos que admita la iteración.
+            //En cada iteración, el valor actual se asigna a la variable valor.
             suma += valor;
         }
         System.out.println("La suma de los valores aleatorios es: " + suma);
