d7e539a4e91aa7c08f2d176d2c6ec47cd5563274
AngelsGV <angels.not99@gmail.com>
2023-12-09 20:14:42 +0100

    Initial commit

---
+++ src/Resuelta5_3.java
@@ -0,0 +1,20 @@
+// Pregunar los números que quiere introducir. Núm enteros y mostrar en orden inverso los números introducidos.
+import java.util.Arrays;
+import java.util.Scanner;
+public class Resuelta5_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca la cantidad de números que quiere la matriz: ");
+        int e= sc.nextInt();
+        int tabla[] = new int[e];
+
+        for(int i= 0; i < e; i++) {
+            System.out.println("Introduzca un número: ");
+            tabla[i]= sc.nextInt();
+        }
+        System.out.println(Arrays.toString(tabla));
+    }
+
+    }
+
+
