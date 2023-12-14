755b64cb29cb8b39486fba19d41c5f9436355ef2
AngelsGV <angels.not99@gmail.com>
2023-12-14 22:39:44 +0100

    Initial commit

--- src/Resuelta5_6.java
+++ src/Resuelta5_6.java
@@ -1,2 +1,14 @@
+// funcion parametros 2 tablas. 1º 6 num 2ºordenada 6 núm. La función devolvera el número de aciertos.
+
+import java.util.Arrays;
+
 public class Resuelta5_6 {
+    static int primitiva (int apuesta[], int comGanadora[]){
+        int aciertos= 0;
+        for (int a:apuesta){
+            if (Arrays.binarySearch(comGanadora, a) >= 0){
+                aciertos ++;
+            }
+        } return (aciertos);
+    }
 }
