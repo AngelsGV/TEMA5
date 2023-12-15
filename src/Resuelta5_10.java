ac3ee6e65548afa408131df31c09828814a4b69d
AngelsGV <angels.not99@gmail.com>
2023-12-16 00:58:51 +0100

    Initial commit

--- src/Resuelta5_10.java
+++ src/Resuelta5_10.java
@@ -1,2 +1,36 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Resuelta5_10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe la lonitud deseada de la tabla: ");
+        int n = sc.nextInt();
+        int t[] = new int[n];
+
+        for(int i=0; i<n; i++ ){
+            System.out.println("Introduce valor (" + i + "):" );
+            t[i]= sc.nextInt();
+        }
+        System.out.println(Arrays.toString(t));
+        System.out.println("¿Qué valor quieres que sea  el máximo en la tabla?");
+        int valor = sc.nextInt();
+
+        sinMayores(t,valor);
+
+    }
+    static int[] sinMayores(int t[],int valor){
+        int copia[] = Arrays.copyOf(t,t.length);
+        int i=0;
+        while (i<copia.length){
+            if (copia[i]>valor){
+                copia[i] = copia[copia.length-1];
+                copia = Arrays.copyOf(copia,copia.length);
+            } else {
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(copia));
+        return copia;
+    }
 }
