39dad43902f295f7ff0f101ec1ce21da4903d128
AngelsGV <angels.not99@gmail.com>
2023-12-18 14:38:19 +0100

    Initial commit

--- src/Aplicación5_14.java
+++ src/Aplicación5_14.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Aplicación5_14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double sueldo;
+        double sueldo = 0;
 
         double contador = 0, sumaSueldos = 0; //Lo inicializamos en 0.
         double sueldos[] = new double[1]; //El sueldo puede ser con decimales.
@@ -13,28 +13,26 @@ public class Aplicación5_14 {
         // Creo que la 1ª opción es la más óptima.
         boolean seguirPreguntando = true; //Lo iniciamos en true porque vamos a poner la condicion en false.
 
-        while (true) {
+        while ( sueldo != -1) {
             System.out.println("Introduce el sueldo (finaliza con -1): ");
             sueldo = sc.nextDouble();
 
             if (sueldo == -1) {
                 seguirPreguntando = false;
+
             if(contador == sueldos.length) {
-                double nueva[] = Arrays.copyOf(sueldos, (int) contador + 1);
+                sueldos = Arrays.copyOf(sueldos, (int) contador + 1);
                 // Se aumenta la longitud de la array.
-                nueva = sueldos; // Se direcciona al mismo sitio de la memoria de la array importante.
+
             }
             sueldos[(int) contador] = sueldo;
                 sumaSueldos += sueldo;
                 contador++;
 
-            }
-            System.out.println("\nSueldos almacenados:");
-            for (int i = 0; i < contador; i++) {
-                System.out.println("Persona " + (i + 1) + ": " + sueldos[i]);
-            }
 
-        }
+
+        }System.out.println(Arrays.toString(sueldos));
 
     }
+}
 }
