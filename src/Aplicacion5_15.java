0921d15b6b95c88a3671ad4a000b0fb25a28c55d
AngelsGV <angels.not99@gmail.com>
2023-12-18 18:39:28 +0100

    Initial commit

--- src/Aplicacion5_15.java
+++ src/Aplicacion5_15.java
@@ -6,10 +6,10 @@ public class Aplicacion5_15 {
         Scanner sc = new Scanner(System.in);
         int[][] notas = new int[5][3];
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Nota del alumno (" + (i + 1) + "): ");
-            for (int j = 0;j < 3; j++){
-                System.out.println("Trimestre " + (j + 1) + ": ");
+        for (int j = 0; j < 3; j++) {
+            System.out.println("Trimestre " + (j + 1) + ": ");
+            for (int i = 0;i < 5; i++){
+                System.out.println("Nota del alumno (" + (i + 1) + "): ");
                 notas[i][j] = sc.nextInt();
             }
         }
