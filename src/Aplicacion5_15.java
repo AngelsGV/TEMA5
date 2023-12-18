64840022ddb0c4cf7c7c87a1899816418ff227fe
AngelsGV <angels.not99@gmail.com>
2023-12-18 18:55:44 +0100

    Initial commit

--- src/Aplicacion5_15.java
+++ src/Aplicacion5_15.java
@@ -1,21 +1,32 @@
 
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Aplicacion5_15 {
-        public static void main(String[]args) {
+
+    public static void main(String[]args) {
+
         Scanner sc = new Scanner(System.in);
+
         int[][] notas = new int[5][3];
 
         for (int j = 0; j < 3; j++) {
             System.out.println("Trimestre " + (j + 1) + ": ");
-            for (int i = 0;i < 5; i++){
+
+            for (int i = 0; i < 5; i++) {
                 System.out.println("Nota del alumno (" + (i + 1) + "): ");
-                notas[i][j] = sc.nextInt();
+                int nota = sc.nextInt();
+                if (nota >= 0 && nota <= 10) {
+                    notas[i][j] = nota;
+                } else {
+                    System.out.println("Nota no valida, vuelve a introducirla: ");
+                }
             }
         }
-
-        }
     }
+}
+
+
 // -------------------------------------------------
 // MEJORAR:
 // - Limitar rango de notas de 0 a 10
